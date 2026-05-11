96bce9650bd2d7c5ed6fb2d641c8967e58aade59
Joe Kenyon <joekenyon72@gmail.com>
2026-05-11 13:00:58 +0100

    good enough

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: 'https://www.josephkenyon.co.uk',
   output: 'static',
-
+  prefetch: true,
   vite: {
     plugins: [tailwindcss()]
   }
