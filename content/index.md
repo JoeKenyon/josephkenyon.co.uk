07336f57428c1d8b0f12bd3c767baf942d4410e9
Joe Kenyon <joekenyon72@gmail.com>
2026-05-16 15:43:55 +0100

    html pretty is what i want idk

--- content/index.md
+++ content/index.md
@@ -6,3 +6,5 @@ layout: home.njk
 title: Home
 ---
 Welcome to my portfolio. I am an artist, musician, and developer.
+
+Explore my [Projects](/projects), listen to my [Music](/music), or browse my [Art Gallery](/art).
--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ import pluginNavigation from "@11ty/eleventy-navigation";
 import pluginImages from "./eleventy.config.images.js";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { HtmlBasePlugin, InputPathToUrlTransformPlugin } from "@11ty/eleventy";
-import htmlmin from "html-minifier-next";
+import prettier from "prettier";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) 
@@ -10,7 +10,7 @@ export default async function(eleventyConfig)
 	
 	// --- PLUGINS ---
 	
-	// Import all the image logic we defined in the other file
+	// import all the image logic we defined in the other file
 	eleventyConfig.addPlugin(pluginImages);
 	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(HtmlBasePlugin);
@@ -19,35 +19,40 @@ export default async function(eleventyConfig)
 
 	// --- ASSETS & WATCHING ---
 	
-	// Copy the 'public' folder (where images are processed to) to the root of _site
-	
-	// Watch and copy CSS files
+	// watch and copy CSS files
 	eleventyConfig.addWatchTarget("css/**/*.css");
 	eleventyConfig.addPassthroughCopy({"css/**/*.css": "/"});
 	
-	// Watch for new images in the content folder to trigger a rebuild
+	// watch for new images in the content folder to trigger a rebuild
 	eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpeg}");
 	eleventyConfig.addPassthroughCopy({"./public/": "/"});
 
-	eleventyConfig.addTransform("beautifyHTML", function(content, outputPath) {
-		// Only run this on .html files
-	if (outputPath && outputPath.endsWith(".html")) {
-		let unminified = htmlmin.minify(content, {
-			removeComments: false,
-			collapseWhitespace: true,
-			conservativeCollapse: true,
-			// This is the magic flag that fixes your indentation:
-			minifyJS: false,
-			minifyCSS: false,
-			caseSensitive: true
-		});
-		
-		// If you want actual formatted pretty printing, you can pipe it to a prettifier, 
-		// or configure html-minifier to keep it structured.
-		return unminified;
+
+	// i want this for some reason
+	eleventyConfig.addTransform("prettierHTML", 
+		function(content, outputPath) 
+		{
+			if (outputPath && outputPath.endsWith(".html")) 
+			{
+				try 
+				{
+					return prettier.format(content, 
+					{ 
+						parser: "html",
+						printWidth: 120,
+						tabWidth: 2,
+						useTabs: false
+					});
+				} 
+				catch (error) 
+				{
+					console.error(outputPath, error);
+					return content;
+				}
+			}
+			return content;
 		}
-		return content;
-	});
+	);
 };
 
 // --- DIRECTORY CONFIG ---
